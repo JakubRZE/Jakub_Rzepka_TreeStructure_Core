e27b61dff75159c7497831cdf2ee65c6762c2c17
Jakub Rzepka <Jakubjakk@gmail.com>
2019-08-02 23:57:45 +0200

    tree style added

--- Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
+++ Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
@@ -1,15 +1,14 @@
 ﻿$(document).ready(() => {
 
-    debugger
+
     loadData();
 
 
     //Slide toggle on click
-
-    //$(document).on("click", (e) => {
-    //    var ulName = e.target.getAttribute('name');
-    //    $("#" + ulName).slideToggle();
-    //});
+    $(document).on("click", ".arrowIcon", (e) => {
+        var ulName = e.target.getAttribute('name');
+        $("#" + ulName).slideToggle();
+    });
 
 
     //$(document).on("click", (e) => {
@@ -18,10 +17,14 @@
     //});
 
    
-    SortList();
+    //SortList();
 
     
-
+    $(function () {
+        $(".sortable").sortable({
+            connectWith: ".connectedSortable"
+        }).disableSelection();
+    });
 });
 
 function loadData() {
@@ -55,24 +58,53 @@ function treeBuilder(parentId, data) {
         {
             if (node.hasChildren == true)
             {
-                appendNode(node, "#" + node.parentNodeId);
+                var parentNodeId = "#" + node.parentNodeId;
+                appendNode(node, parentNodeId);
             }
             else
             {
-                var nodeHtml = `<li id="${node.id}" class="${node.parentNodeId}"> ${node.name} </li>`;
+                var nodeHtml =
+                    //`<li id="${node.id}" class="ui-state-default m-0 p-0 ${node.parentNodeId}"> ${node.name} </li>`;
+                    `<li  id="${node.id}" class="ui-state-default m-0 p-0 ${node.parentNodeId}">
+                         <div class="flexbox">         
+                            <div class="d-inline pl-2">
+                                <span style="font-size: 0.3em; vertical-align: middle;">
+                                   <i class="fas fa-circle" style="vertical-align: middle;"></i> 
+                                </span>
+                            </div>
+
+                            <div class="d-inline customElement pl-2">
+                                Item 3
+                            </div>
+                        </div>
+                    </li>`;
                 $("#" + node.parentNodeId).append(nodeHtml);
             }
         }
-
         treeBuilder(node.id, data);
     }
 }
 
 function appendNode(node, appendTo) {
-    var nodeHtml = `<li class="${node.parentNodeId}"  name="${node.id}"> ${node.name}
-                       <ul id="${node.id}">    
-                       </ul>
-                    </li>`;
+    var nodeHtml =
+        //`<li class="ui-state-default ${node.parentNodeId}"  name="${node.id}"> ${node.name}
+        //               <ul id="${node.id}" class="sortable">    
+        //               </ul>
+        //            </li>`
+        `<li class="ui-state-default m-0 p-0 ${node.parentNodeId}" name="${node.id}">
+            <div class="flexbox">
+                <div class="d-inline ">
+                    <i class="fas fa-caret-right text-center arrowIcon" name="${node.id}"></i>
+                </div>
+
+                <div class="d-inline customElement pl-1">
+                   ${node.name}
+                </div>
+            </div>
+
+            <ul id="${node.id}" class="sortable customUlist connectedSortable">
+            </ul>
+        </li>`;
     $(appendTo).append(nodeHtml);
 }
 
