76d7b38291612c5fa3ed493f537ce84ef4d7bf94
Jakub Rzepka <Jakubjakk@gmail.com>
2019-08-04 00:25:02 +0200

    hide/show tree added

--- Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
+++ Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
@@ -2,15 +2,6 @@
 
     loadData();
 
-    //Slide toggle on click
-    $(document).on("click", ".arrowIcon", (e) => {
-        var ulId = "#" + e.target.getAttribute('name');
-        $(ulId).slideToggle();
-        $(e.target).toggleClass("flip");
-    });
-
-    //SortList();
-
 });
 
 function loadData() {
@@ -24,7 +15,13 @@ function loadData() {
         success: (data) => {
 
             treeBuilder(null, data);
+
             MakeListSortable();
+            SlideToggleList();
+            SortList();
+            ShowHideDragIcon();
+            MarkAsActive();
+            ShowHideLists();
 
         },
         error: (xhr, ajaxOptions, thrownError) => {
@@ -48,18 +45,31 @@ function treeBuilder(parentId, data) {
             }
             else {
                 var nodeHtml =
-                    `<li  id="${node.id}" class="ui-state-default m-0 p-0 ${node.parentNodeId}">
+
+
+
+
+
+                    `<li class="ui-state-default m-0 p-0 ${node.parentNodeId} name="${node.id}">
+
                          <div class="flexbox">         
+
                             <div class="d-inline pl-2">
-                               
-
                             </div>
+                            <input class="w-100 customInput" name="${node.id}" value="${node.name}" readonly="">
 
-                            <div class="d-inline customElement pl-3">
-                                Item 3
-                            </div>
+                            <span class="pr-1 movable" name="drag${node.id}" style="font-size: 1em; background-color: #ffc107; display:none;">
+                            <i class="fas fa-arrows-alt"></i>
+                            <span>
+
                         </div>
+
                     </li>`;
+
+
+
+
+
                 $("#" + node.parentNodeId).append(nodeHtml);
             }
         }
@@ -67,55 +77,85 @@ function treeBuilder(parentId, data) {
     }
 }
 
-//<span style="font-size: 0.3em; vertical-align: middle;">
-//    <i class="fas fa-circle" style="vertical-align: middle;"></i>
-//</span>
 
 function appendNode(node, appendTo) {
     var nodeHtml =
+
+
+
+
+
         `<li class="ui-state-default m-0 p-0 ${node.parentNodeId}" name="${node.id}">
+
             <div class="flexbox">
+
                 <div class="d-inline ">
                     <i class="fas fa-caret-down text-center arrowIcon" name="${node.id}"></i>
                 </div>
-                <div class="d-inline customElement pl-1">
-                   ${node.name}
-                </div>
+
+                <input class="w-100 customInput" name="${node.id}" value="${node.name}" readonly="">
+
+                <span class="pr-1 movable" name="drag${node.id}" style="font-size: 1em; background-color: #ffc107; display:none;">
+                <i class="fas fa-arrows-alt"></i>
+                <span>
+
             </div>
-            <ul id="${node.id}" class="sortable customUlist connectedSortable">
+
+
+
+
+
+
+            <ul id="${node.id}" class="sortable customUlist connectedSortable toggle">
             </ul>
+
         </li>`;
     $(appendTo).append(nodeHtml);
 }
 
 
-
-function SortList() {
-    $(document).on("click", (e) => {
-        var listId = "#" + e.target.getAttribute('name');
-
-        sorting(listId);
-        $(listId).toggleClass('desc');
+function SlideToggleList() {
+    $(document).on("click", ".arrowIcon", (e) => {
+        var ulId = "#" + e.target.getAttribute('name');
+        $(ulId).slideToggle();
+        $(e.target).toggleClass("flip");
     });
 }
 
-function sorting(listId) {
+function SortList() {
+    $(document).on("click", "#Sort", () => {
+        var listId = "#" + $(".Active").attr('name'); 
+        SortingEngine(listId);
+        $(listId).toggleClass('desc');
+});
+}
 
+function ShowHideLists() {
+    $(document).on("click", "#Roll", (e) => {
+        if (!$(e.target).hasClass("clicked")) {
+                $(e.target).addClass("clicked");
+                $("ul:not(.wrap)").slideUp();
+                $(".fa-caret-down").addClass("flip");
+        }
+        else {
+            $(e.target).removeClass("clicked");
+            $("ul:not(.wrap)").slideDown();
+            $(".fa-caret-down").removeClass("flip");
+        }
+    });
+}
+
+function SortingEngine(listId) {
     $(listId).html(
-
         $(listId).children("li").sort(function (a, b) {
-
             if (!$(listId).hasClass("desc")) {
                 return $(a).text().toUpperCase().localeCompare(
-
                     $(b).text().toUpperCase());
             }
             else {
                 return -$(a).text().toUpperCase().localeCompare(
-
                     $(b).text().toUpperCase());
             }
-
         })
     );
 
@@ -123,8 +163,30 @@ function sorting(listId) {
 
 function MakeListSortable() {
     $(".sortable").sortable({
-        connectWith: ".connectedSortable"
+        connectWith: ".connectedSortable",
     }).disableSelection();
 }
 
+function ShowHideDragIcon() {
+    $(document).on("click", "input", (e) => {
+        var inputName = e.target.getAttribute('name');
+        $('[name=drag' + inputName + ']').show();
 
+        $(e.target).focusout(() => {
+            $('[name=drag' + inputName + ']').hide();
+        })
+    });
+}
+
+function MarkAsActive() {
+    $(document).on("click", "input", (e) => {
+        if (!$(e.target).hasClass("Active")) {
+            $(".Active").removeClass("Active")
+            $(e.target).addClass("Active");
+        }
+        else {
+            $(".Active").removeClass("Active")
+            $('input').blur();
+        }
+    });
+}
