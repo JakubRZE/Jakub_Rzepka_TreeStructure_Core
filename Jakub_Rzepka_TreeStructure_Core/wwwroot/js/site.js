aa3e965d9e28a2bd6f2f89d277f336635c5a9c20
Jakub Rzepka <Jakubjakk@gmail.com>
2019-08-04 17:48:38 +0200

    code refactoring

--- Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
+++ Jakub_Rzepka_TreeStructure_Core/wwwroot/js/site.js
@@ -32,87 +32,37 @@ function loadData() {
 };
 
 function treeBuilder(parentId, data) {
-
     for (var node of data.filter(x => x.parentNodeId === parentId)) {
 
-        if (node.parentNodeId === null) {
-            appendNode(node, '.wrap');
-        }
-        else {
-            if (node.hasChildren == true) {
-                var parentNodeId = "#" + node.parentNodeId;
-                appendNode(node, parentNodeId);
-            }
-            else {
-                var nodeHtml =
+        var appendTo = parentId === null ? '.wrap' : "#" + node.parentNodeId;
+        var nodeHtml = GenerateHTML(node.name, node.id, node.parentNodeId, node.hasChildren);
 
+        $(appendTo).append(nodeHtml);
 
-
-
-
-                    `<li class="ui-state-default m-0 p-0 ${node.parentNodeId} name="${node.id}">
-
-                         <div class="flexbox">         
-
-                            <div class="d-inline pl-2">
-                            </div>
-                            <input class="w-100 customInput" name="${node.id}" value="${node.name}" readonly="">
-
-                            <span class="pr-1 movable" name="drag${node.id}" style="font-size: 1em; background-color: #ffc107; display:none;">
-                            <i class="fas fa-arrows-alt"></i>
-                            <span>
-
-                        </div>
-
-                    </li>`;
-
-
-
-
-
-                $("#" + node.parentNodeId).append(nodeHtml);
-            }
-        }
         treeBuilder(node.id, data);
     }
 }
 
+function GenerateHTML(nodeName, nodeId, parentNodeId, hasChildren) {
 
-function appendNode(node, appendTo) {
-    var nodeHtml =
+    var arrowIcon = hasChildren ? `<i class="fas fa-caret-down text-center arrowIcon" name="${nodeId}"></i>` : "";
+    var anotherList = hasChildren ? `<ul id="${nodeId}" class="sortable customUlist connectedSortable toggle"> </ul>` : "";
 
+    var htmlForAppend = `<li class="ui-state-default m-0 p-0 ${parentNodeId} name="${nodeId}">
+                             <div class="flexbox">         
+                                <div class="d-inline pl-2">
+                                     ${arrowIcon}
+                                </div>
+                                <input class="w-100 customInput" name="${nodeId}" value="${nodeName}" readonly="">
+                                <span class="pr-1 movable" name="drag${nodeId}" style="font-size: 1em; background-color: #ffc107; display:none;">
+                                     <i class="fas fa-arrows-alt"></i>
+                                <span>
+                            </div>
+                             ${anotherList}
+                        </li>`;
 
-
-
-
-        `<li class="ui-state-default m-0 p-0 ${node.parentNodeId}" name="${node.id}">
-
-            <div class="flexbox">
-
-                <div class="d-inline ">
-                    <i class="fas fa-caret-down text-center arrowIcon" name="${node.id}"></i>
-                </div>
-
-                <input class="w-100 customInput" name="${node.id}" value="${node.name}" readonly="">
-
-                <span class="pr-1 movable" name="drag${node.id}" style="font-size: 1em; background-color: #ffc107; display:none;">
-                <i class="fas fa-arrows-alt"></i>
-                <span>
-
-            </div>
-
-
-
-
-
-
-            <ul id="${node.id}" class="sortable customUlist connectedSortable toggle">
-            </ul>
-
-        </li>`;
-    $(appendTo).append(nodeHtml);
+    return htmlForAppend;
 }
-
 
 function SlideToggleList() {
     $(document).on("click", ".arrowIcon", (e) => {
@@ -137,14 +87,12 @@ function MarkAsActive() {
 
 function SortList() {
     $(document).on("click", "#Sort", () => {
-        if (!$("input").hasClass("Active"))
-        {
-            var listClass =".wrap"
+        if (!$("input").hasClass("Active")) {
+            var listClass = ".wrap"
             SortingEngine(listClass);
             $(listClass).toggleClass('desc');
         }
-        else
-        {
+        else {
             var listId = "#" + $(".Active").attr('name');
             SortingEngine(listId);
             $(listId).toggleClass('desc');
@@ -152,7 +100,6 @@ function SortList() {
 
     });
 }
-
 
 function SortingEngine(listId) {
     $(listId).html(
@@ -204,3 +151,31 @@ function ShowHideDragIcon() {
     });
 }
 
+function AddNewNode() {
+
+    $(document).on("click", "#Add", () => {
+
+        if (!$("input").hasClass("Active")) {
+
+            var listClass = ".wrap"
+
+            Add(listClass);
+
+
+
+        }
+        else {
+
+            var listId = "#" + $(".Active").attr('name');
+
+            SortingEngine(listId);
+
+
+        }
+
+    });
+}
+
+function AddNode() {
+
+}
